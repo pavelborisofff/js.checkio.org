7267f5eb59165e8b2f6d823b954706251461d97c
pavelborisov <pavel.borisofff@gmail.com>
2021-02-20 12:21:54 +0300

    deleted scrap

--- Elementary/correctSentence.js
+++ Elementary/correctSentence.js
@@ -9,7 +9,6 @@ function newFunction() {
 }
 
 function correctSentence(text) {
-    text[0].toUpperCase() + text.slice(1) + (text.slice(-1) == '.' ? '' : '.')
     return text[0].toUpperCase() + text.slice(1) + (text.slice(-1) == '.' ? '' : '.');
 }
 
